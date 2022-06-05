88fe4250c17301d17f4fde27f101aaba137fb953
Forever Young <dy9040@gmail.com>
2022-06-05 01:58:47 -0500

    got post fuctioning

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const { notes } =require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+
+    res.json(results);
+});
 
 
 app.get('/', (req, res) => {
@@ -12,6 +25,37 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+
+function createNewNote(body, notesList) {
+    const note = body;
+    notesList.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesList }, null, 2)
+    );
+    return note;
+}
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if(!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+}
 
 
 app.listen(PORT, () =>
